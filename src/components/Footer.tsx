977d9da77c684d1f6df12d82219523da7ba1f957
Kyle Hensman <47787136+kyle-eastcode@users.noreply.github.com>
2023-11-02 17:33:08 -0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,3 @@
-import { SLOGAN, TITLE } from '@/config/contants'
-
 export default function Footer() {
   const year = new Date().getFullYear()
 
@@ -7,10 +5,10 @@ export default function Footer() {
     <footer className='flex flex-col gap-8 justify-center items-center py-12'>
       <div className='text-6xl'>🌍</div>
       <div className='flex flex-col text-center md:flex-row md:text-left'>
-        <p>&#169; {TITLE} {year}</p>
+        <p>&#169; Culture DAO {year}</p>
         <div className='mx-2 hidden md:block'>{' - '}</div>
         <div className='my-1 md:hidden'></div>
-        <p>{SLOGAN}</p>
+        <p>Reimagining Blockchain Communities</p>
       </div>
     </footer>
   )
