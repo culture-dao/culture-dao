ced2dc5d09f4c6d6c0fff976a94c6fefb2907e74
Kyle Hensman <kyle@eastcode.ca>
2023-11-03 14:57:52 -0300

    style(client): update styles and add animations

--- src/components/Journey.tsx
+++ src/components/Journey.tsx
@@ -2,10 +2,10 @@ import Image from 'next/image'
 
 export default function Journey() {
   return (
-    <section className='flex flex-col gap-8 justify-center items-center py-32'>
+    <section className='flex flex-col gap-8 justify-center items-center'>
       <h3 className='uppercase text-2xl text-center'>Join the Journey</h3>
-      <ul className='flex w-1/4 justify-around items-center'>
-        <li className='flex flex-col justify-center items-center'>
+      <ul className='flex w-full md:w-1/4 justify-around px-8'>
+        <li className='relative'>
           <Image
             className='rounded-md mx-auto mb-4'
             src="https://cdnjs.cloudflare.com/ajax/libs/twemoji/14.0.2/svg/1f517.svg"
@@ -15,7 +15,7 @@ export default function Journey() {
           />
           <h4>Culture</h4>
         </li>
-        <li className='flex flex-col justify-center items-center'>
+        <li className='relative'>
           <Image
             className='rounded-md mx-auto mb-4'
             src="https://cdnjs.cloudflare.com/ajax/libs/twemoji/14.0.2/svg/1f4ab.svg"
@@ -25,7 +25,7 @@ export default function Journey() {
           />
           <h4>DAO</h4>
         </li>
-        <li className='flex flex-col justify-center items-center'>
+        <li className='relative'>
           <Image
             className='rounded-md mx-auto mb-4'
             src="https://cdnjs.cloudflare.com/ajax/libs/twemoji/14.0.2/svg/1f465.svg"
